2e7a97f16d7b045ee0c9160d9e4c1dcdeadc352e
souiiii <b423037@iiit-bh.ac.in>
2026-03-08 15:29:51 +0530

    basic server setup

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log("server started"));
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.use((req, res) =>
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html")),
+);
+
+server.listen(PORT, () => console.log("server started"));
